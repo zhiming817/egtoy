0d2526a22b56f04f5d4e594cc4b608d1c26f8b2a
zhiming <zhiming817@gmail.com>
2025-05-16 00:08:44 +0800

    change android ndk version

--- frontend/egtoy-app/android/app/build.gradle.kts
+++ frontend/egtoy-app/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.egtoy.egtoy"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13113456"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
